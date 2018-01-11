346f8b3269c72913c91f8f1050c7e99aec75b237
victorzubov1993 <victor.zubov1993@gmail.com>
2018-01-12 00:37:08 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,8 +2,8 @@
  * Contructor for my JS app
  */
 var tintupTweetApp = function () {
-    this.baseUrl = "https://guarded-wave-7955.herokuapp.com/";
-    this.modelsBaseUrl = "https://guarded-wave-7955.herokuapp.com/models/";
+    this.baseUrl = "https://sentimentanalys.herokuapp.com/";
+    this.modelsBaseUrl = "https://sentimentanalys.herokuapp.com/models/";
     this.sentimentColorMap = {
         'pos': 'green',
         'neg': 'red',
