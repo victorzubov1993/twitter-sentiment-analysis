3e8845692f0f9e0aab161f0dea34aecb55a6ffe6
victorzubov1993 <victor.zubov1993@gmail.com>
2018-01-12 00:36:04 +0200

    Update config.php

--- conf/config.php
+++ conf/config.php
@@ -1,8 +1,8 @@
 <?php
 $twitter_api_conf = array(
-    'oauth_access_token' => "",
-    'oauth_access_token_secret' => "",
-    'consumer_key' => "",
-    'consumer_secret' => ""
+    'oauth_access_token' => "951531949616783361-oIccKr9S2tJMTJOrwJLaDESC9e28wkx",
+    'oauth_access_token_secret' => "Woc6FhPBvoUf7vIDSy6HoOhEhjEF6t0uErDjHXDt0kyBr",
+    'consumer_key' => "pUhcIXyzCzFdqcww5WS3OVvpb",
+    'consumer_secret' => "oCTEQyzwQfJlQJxNA3pTuWINOlo4wWclCVp6dwLM7GVzqhd8vI"
 );
 
